e602c3fbd6a845be40c5f37f8fc0f8c5c3462fef
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-02-26 16:04:53 +0100

    fetching data from the api

--- snack1/snack1.js
+++ snack1/snack1.js
@@ -5,3 +5,10 @@ Ottieni il titolo di un post con una Promise.
 Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
 
 */
+
+const id = 1;
+
+fetch(`https://dummyjson.com/posts/${id}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
