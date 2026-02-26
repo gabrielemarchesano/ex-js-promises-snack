3b8f671aa4505811d69e0e314f09e7fc6f598840
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-02-26 14:25:36 +0100

    first commit

---
+++ snack1/snack1.js
@@ -0,0 +1,7 @@
+/* Snack 1
+
+Ottieni il titolo di un post con una Promise.
+
+Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+
+*/
